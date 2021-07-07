5b02ae55f602e0478c427eac46e0df3e9974a647
parkyoula <ouneno@naver.com>
2021-07-08 00:22:28 +0900

    Academy : 테스트 네번째

---
+++ youlatest/src/main/java/academy/Academy2.java
@@ -0,0 +1,9 @@
+package academy;
+
+public class Academy2 {
+
+	public static void main(String[] args) {
+		System.out.println("2021-07-08 12:21 아카데미테스트");
+	}
+
+}
---
+++ youlatest/src/test/java/academy/AcademyTest2.java
@@ -0,0 +1,9 @@
+package academy;
+
+public class AcademyTest2 {
+
+	public static void main(String[] args) {
+		System.out.println("테스트 집에서 첫번째");
+	}
+
+}

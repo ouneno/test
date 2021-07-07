f1771befc9a3722ff876518e033c606131d15356
bit <bit@DESKTOP-INJFT3B>
2021-07-07 19:59:30 +0900

    Create

---
+++ youlatest/src/main/java/academy/Academy.java
@@ -0,0 +1,9 @@
+package academy;
+
+public class Academy {
+
+	public static void main(String[] args) {
+		System.out.println("2021-07-07 19:57");
+	}
+
+}
---
+++ youlatest/src/test/java/academy/AcademyTest.java
@@ -0,0 +1,9 @@
+package academy;
+
+public class AcademyTest {
+
+	public static void main(String[] args) {
+		System.out.println("2021-07-07 19:58");
+	}
+
+}

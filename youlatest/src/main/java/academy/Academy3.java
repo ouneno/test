17dec0d1718b2df8a35c218db44c282ccd0a4a16
parkyoula <ouneno@naver.com>
2021-07-08 00:28:58 +0900

    Academy : 다섯번째 테스트

---
+++ youlatest/src/main/java/academy/Academy3.java
@@ -0,0 +1,9 @@
+package academy;
+
+public class Academy3 {
+
+	public static void main(String[] args) {
+		System.out.println("다섯번째 테스트 해볼만하네");
+	}
+
+}
---
+++ youlatest/src/test/java/academy/AcademyTest3.java
@@ -0,0 +1,9 @@
+package academy;
+
+public class AcademyTest3 {
+
+	public static void main(String[] args) {
+		System.out.println("다섯번째 테스트");
+	}
+
+}
